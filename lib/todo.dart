dbf5f153092431f630cd2896837d91d2f674f68e
Jirapa Ramjan <ramjan_j@silpakorn.edu>
2022-10-11 19:00:16 +0700

    my to do

--- lib/todo.dart
+++ lib/todo.dart
@@ -11,9 +11,9 @@ class ToDo {
 
   static List<ToDo> todoList() {
     return [
-      ToDo(id: '01', todoText: 'ทำการบ้านครั้งที่ 4 วิชา Mobile App Dev', isDone: true ),
+      ToDo(id: '01', todoText: 'ทำโปรเจ็ควิชา Mobile App Dev' ),
       ToDo(id: '02', todoText: 'ทำการบ้านครั้งที่ 5 วิชา Mobile App Dev',),
-      ToDo(id: '03', todoText: 'ทำโปรเจ็ควิชา Mobile App Dev', ),
+      ToDo(id: '03', todoText: 'ทำการบ้านครั้งที่ 4 วิชา Mobile App Dev', isDone: true, ),
     ];
   }
 }
